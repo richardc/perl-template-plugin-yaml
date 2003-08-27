17ec55f79fb47670493e757a48f6ff31a6aef47f
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2003-08-27 16:19:24 +0000

    applied from merlyn, plus interleave docs
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Template-Plugin-YAML@1510 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Template/Plugin/YAML.pm
+++ lib/Template/Plugin/YAML.pm
@@ -2,16 +2,68 @@ use strict;
 package Template::Plugin::YAML;
 use Template::Plugin;
 use base 'Template::Plugin';
-use YAML qw(Dump);
+use YAML qw(Dump Load DumpFile LoadFile);
 use vars qw($VERSION);
 $VERSION = 1.21;
 
-# any resemblance to Template::Plugin::Dumper is because we stole it all
+
+=head1 NAME
+
+Template::Plugin::YAML - Plugin interface to YAML
+
+=head1 SYNOPSIS
+
+    [% USE YAML %]
+
+    [% YAML.dump(variable) %]
+    [% YAML.dump_html(variable) %]
+    [% value = YAML.loadyaml(yaml_string) %]
+    [% YAML.dumpfile(filename, variable) %]
+    [% value = YAML.loadfile(filename) %]
+
+=head1 DESCRIPTION
+
+This is a simple Template Toolkit Plugin Interface to the YAML module.
+A YAML object will be instantiated via the following directive:
+
+    [% USE YAML %]
+
+As a standard plugin, you can also specify its name in lower case:
+
+    [% USE yaml %]
+
+=head1 METHODS
+
+These are the methods supported by the YAML object.
+
+=head2 dump( @variables )
+
+Generates a raw text dump of the data structure(s) passed
+
+    [% USE Dumper %]
+    [% yaml.dump(myvar) %]
+    [% yaml.dump(myvar, yourvar) %]
+
+=cut
+
 sub dump {
     my $self = shift;
     my $content = Dump @_;
     return $content;
 }
+
+
+=head2 dump_html( @variables )
+
+Generates a dump of the data structures, as per C<dump>, but with the
+characters E<lt>, E<gt> and E<amp> converted to their equivalent HTML
+entities, spaces converted to &nbsp; and newlines converted to
+E<lt>brE<gt>.
+
+    [% USE yaml %]
+    [% yaml.dump_html(myvar) %]
+
+=cut
 
 sub dump_html {
     my $self = shift;
@@ -26,60 +78,61 @@ sub dump_html {
     return $content;
 }
 
+
+=head2 loadyaml( $string )
+
+Converts a YAML-encoded string into the equivalent data structure.
+Here's a way to deep-copy a complex structure by completely serializing
+the data.
+
+  [% USE yaml;
+     yaml_string = yaml.dump(complex_data_structure);
+     complex_copy = yaml.loadyaml(yaml_string);
+  %]
+
+=cut
+
+sub loadyaml {
+    my $self = shift;
+    return Load shift;
+}
+
+
+=head2 dumpfile( $file, @variables )
+
+Converts the data to YAML encoding, and dumps it to the specified
+filepath.
+
+  [% USE yaml; yaml.dumpfile(".storage", my_data) %]
+
+=cut
+
+sub dumpfile {
+    my $self = shift;
+    return DumpFile @_;
+}
+
+
+=head2 loadfile( $file )
+
+Loads the YAML-encoded data from the specified filepath
+
+  [% USE yaml; my_data = yaml.loadfile(".storage") %]
+
+=cut
+
+sub loadfile {
+    my $self = shift;
+    return LoadFile @_;
+}
+
 1;
 __END__
 
-=head1 NAME
+=head1 AUTHORS
 
-Template::Plugin::YAML - Plugin interface to YAML
-
-=head1 SYNOPSIS
-
-    [% USE YAML %]
-
-    [% YAML.dump(variable) %]
-    [% YAML.dump_html(variable) %]
-
-=head1 DESCRIPTION
-
-This is a very simple Template Toolkit Plugin Interface to the YAML
-module.  A YAML object will be instantiated via the following
-directive:
-
-    [% USE YAML %]
-
-As a standard plugin, you can also specify its name in lower case:
-
-    [% USE yaml %]
-
-=head1 METHODS
-
-There are two methods supported by the YAML object.  Each will
-output into the template the contents of the variables passed to the
-object method.
-
-=head2 dump()
-
-Generates a raw text dump of the data structure(s) passed
-
-    [% USE Dumper %]
-    [% yaml.dump(myvar) %]
-    [% yaml.dump(myvar, yourvar) %]
-
-
-=head2 dump_html()
-
-Generates a dump of the data structures, as per dump(), but with the
-characters E<lt>, E<gt> and E<amp> converted to their equivalent HTML
-entities, spaces converted to &nbsp; and newlines converted to
-E<lt>brE<gt>.
-
-    [% USE yaml %]
-    [% yaml.dump_html(myvar) %]
-
-=head1 AUTHOR
-
-Richard Clamp <richardc@unixbeard.net>
+Richard Clamp <richardc@unixbeard.net>, with load, loadfile, and
+dumpfile implementation by Randal L. Schwartz <merlyn@stonehenge.com>
 
 based on Simon Matthews' L<Template::Plugin::Dumper>
 
